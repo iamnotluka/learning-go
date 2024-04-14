0cf26e97016e234897b53cad0245ac90e4832fb7
iamnotluka <zoricluk@gmail.com>
2024-04-15 08:36:56 +1000

    Maps

--- youtube-tut/main.go
+++ youtube-tut/main.go
@@ -158,6 +158,33 @@ func main() {
 	}
 
 	showScore()
+
+	// MAPS
+	menu := map[string]float64{
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"toffee pudding": 3.55,
+	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["soup"])
+
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// Integers as key type
+	phonebook := map[int]string{
+		1: "luka",
+		2: "relja",
+		3: "tamara",
+	}
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[1])
+
+	phonebook[1] = "luka_changed"
+	fmt.Println(phonebook)
 }
 
 func sayGreeting(n string) {
